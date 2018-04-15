af6f36b89b05bd47bbfa66490348113cef742503
Anderson Laurentino <alaurentino.br@gmail.com>
2018-04-14 21:36:19 -0300

    Add Card Component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+// import './Card.css'
+
+class EnvironmentPage extends Component {
+
+    render() {
+        return (
+            <div className="card">
+                <div className="card-header">
+                    {this.props.title}
+                </div>
+                <div className="card-body">
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default EnvironmentPage
--- src/components/EnvironmentPage/EnvironmentPage.jsx
+++ src/components/EnvironmentPage/EnvironmentPage.jsx
@@ -4,6 +4,32 @@ import $ from 'jquery'
 
 import './EnvironmentPage.css'
 import Sidebar from '../Sidebar/Sidebar';
+import Card from '../Card/Card'
+
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+        label: 'Economy in $',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(7, 71, 166, 1)',
+        borderColor: 'rgba(7, 71, 166, 1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(7, 71, 166, 1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(7, 71, 166, 1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40]
+    }]
+};
 
 class EnvironmentPage extends Component {
 
@@ -26,8 +52,11 @@ class EnvironmentPage extends Component {
                     <div className="container">
                         <div className="content">
                             <div className="env-graph">
-                                <Line
-                                    height={90}/>
+                                <Card title="Economy">
+                                    <Line
+                                        data={data}
+                                        height={90}/>
+                                </Card>
                             </div>
                         </div>
                     </div>
